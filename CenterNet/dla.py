373336118638a77b374ba682c6b26dc9eb27e983
calmisential <xinyu_std@163.com>
2021-12-04 22:19:45 +0800

    Build dla network

--- CenterNet/dla.py
+++ CenterNet/dla.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -11,7 +12,7 @@ class BasicBlock(nn.Module):
     def __init__(self, c_in, c_out, stride=1):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=c_in, out_channels=c_out, kernel_size=(3, 3), stride=(stride, stride),
-                               padding=same_pad(3, stride), bias=False)
+                               padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(num_features=c_out)
         self.conv2 = nn.Conv2d(in_channels=c_out, out_channels=c_out, kernel_size=(3, 3), stride=(1, 1),
                                padding=1, bias=False)
@@ -37,7 +38,7 @@ class BottleNeck(nn.Module):
                                padding=0, bias=False)
         self.bn1 = nn.BatchNorm2d(num_features=c_t)
         self.conv2 = nn.Conv2d(in_channels=c_t, out_channels=c_t, kernel_size=(3, 3), stride=(stride, stride),
-                               padding=same_pad(3, stride), bias=False)
+                               padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(num_features=c_t)
         self.conv3 = nn.Conv2d(in_channels=c_t, out_channels=c_out, kernel_size=(1, 1), stride=(1, 1),
                                padding=0, bias=False)
@@ -63,7 +64,7 @@ class BottleNeckX(nn.Module):
                                padding=0, bias=False)
         self.bn1 = nn.BatchNorm2d(num_features=c_t)
         self.conv2 = nn.Conv2d(in_channels=c_t, out_channels=c_t, kernel_size=(3, 3), stride=(stride, stride),
-                               padding=same_pad(3, stride), groups=BottleNeckX.cardinality, bias=False)
+                               padding=1, groups=BottleNeckX.cardinality, bias=False)
         self.bn2 = nn.BatchNorm2d(num_features=c_t)
         self.conv3 = nn.Conv2d(in_channels=c_t, out_channels=c_out, kernel_size=(1, 1), stride=(1, 1),
                                padding=0, bias=False)
@@ -82,7 +83,8 @@ class BottleNeckX(nn.Module):
 class Root(nn.Module):
     def __init__(self, c_in, c_out, residual):
         super(Root, self).__init__()
-        self.conv = nn.Conv2d(in_channels=c_in, out_channels=c_out, kernel_size=(1, 1), stride=(1, 1), padding=0, bias=False)
+        self.conv = nn.Conv2d(in_channels=c_in, out_channels=c_out, kernel_size=(1, 1), stride=(1, 1), padding=0,
+                              bias=False)
         self.bn = nn.BatchNorm2d(num_features=c_out)
         self.residual = residual
 
@@ -157,7 +159,8 @@ class DLA(nn.Module):
         self.num_classes = num_classes
 
         self.base_layer = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=channels[0], kernel_size=(7, 7), stride=(1, 1), padding=3, bias=False),
+            nn.Conv2d(in_channels=3, out_channels=channels[0], kernel_size=(7, 7), stride=(1, 1), padding=3,
+                      bias=False),
             nn.BatchNorm2d(num_features=channels[0]),
             nn.ReLU()
         )
@@ -177,35 +180,23 @@ class DLA(nn.Module):
                             out_channels=channels[5], stride=2,
                             level_root=True, root_residual=residual_root)
         self.avgpool = nn.AvgPool2d(kernel_size=pool_size)
-        self.final = nn.Conv2d(in_channels=channels[5], out_channels=num_classes, kernel_size=(1, 1), stride=(1, 1), padding=0, bias=True)
-
+        self.final = nn.Conv2d(in_channels=channels[5], out_channels=num_classes, kernel_size=(1, 1), stride=(1, 1),
+                               padding=0, bias=True)
 
     @staticmethod
     def _make_conv_level(in_channels, out_channels, convs, stride=1):
         layers = []
         for i in range(convs):
-            if i == 0:
-                layers.extend([
-                    nn.Conv2d(in_channels=in_channels,
-                              out_channels=out_channels,
-                              kernel_size=(3, 3),
-                              stride=(stride, stride),
-                              padding=same_pad(3, stride),
-                              bias=False),
-                    nn.BatchNorm2d(num_features=out_channels),
-                    nn.ReLU()
-                ])
-            else:
-                layers.extend([
-                    nn.Conv2d(in_channels=out_channels,
-                              out_channels=out_channels,
-                              kernel_size=(3, 3),
-                              stride=(1, 1),
-                              padding=1,
-                              bias=False),
-                    nn.BatchNorm2d(num_features=out_channels),
-                    nn.ReLU()
-                ])
+            layers.extend([
+                nn.Conv2d(in_channels=in_channels,
+                          out_channels=out_channels,
+                          kernel_size=(3, 3),
+                          stride=stride if i == 0 else 1,
+                          padding=1,
+                          bias=False),
+                nn.BatchNorm2d(num_features=out_channels),
+                nn.ReLU()
+            ])
         return nn.Sequential(*layers)
 
     def forward(self, x):
@@ -242,7 +233,7 @@ class Identity(nn.Module):
 
 
 class IDAUp(nn.Module):
-    def __init__(self, in_dim, node_kernel, out_dim, channels, up_factors):
+    def __init__(self, node_kernel, out_dim, channels, up_factors):
         super(IDAUp, self).__init__()
         self.channels = channels
         self.out_dim = out_dim
@@ -251,7 +242,7 @@ class IDAUp(nn.Module):
                 proj = Identity()
             else:
                 proj = nn.Sequential(
-                    nn.Conv2d(in_channels=in_dim,
+                    nn.Conv2d(in_channels=c,
                               out_channels=out_dim,
                               kernel_size=(1, 1),
                               stride=(1, 1),
@@ -264,31 +255,118 @@ class IDAUp(nn.Module):
             if f == 1:
                 up = Identity()
             else:
-                up = nn.ConvTranspose2d(in_channels=out_dim, out_channels=out_dim, kernel_size=f*2, stride=f, padding=f, output_padding=f, groups=out_dim, bias=False)
+                up = nn.ConvTranspose2d(in_channels=out_dim, out_channels=out_dim, kernel_size=f * 2, stride=f,
+                                        padding=f // 2, output_padding=0, groups=out_dim, bias=False)
             setattr(self, "proj_" + str(i), proj)
             setattr(self, "up_" + str(i), up)
         for i in range(1, len(channels)):
             node = nn.Sequential(
-                nn.Conv2d(in_channels=(f+1)*out_dim, out_channels=out_dim, kernel_size=node_kernel, strides=1, padding=same_pad(node_kernel, 1), bias=False),
+                nn.Conv2d(in_channels=out_dim * 2, out_channels=out_dim, kernel_size=node_kernel, stride=1,
+                          padding=same_pad(node_kernel, 1), bias=False),
                 nn.BatchNorm2d(num_features=out_dim),
                 nn.ReLU()
             )
             setattr(self, "node_" + str(i), node)
 
     def forward(self, inputs):
-        pass
+        layers = list(inputs)
+        for i, l in enumerate(layers):
+            upsample = getattr(self, "up_" + str(i))
+            project = getattr(self, "proj_" + str(i))
+            layers[i] = upsample(project(l))
+        x = layers[0]
+        y = []
+        for i in range(1, len(layers)):
+            node = getattr(self, "node_" + str(i))
+            x = node(torch.cat([x, layers[i]], 1))
+            y.append(x)
+        return x, y
+
+
+class DLAUp(nn.Module):
+    def __init__(self, channels, scales=(1, 2, 4, 8, 16), in_channels=None):
+        super(DLAUp, self).__init__()
+        if in_channels is None:
+            in_channels = channels
+        self.channels = channels
+        channels = list(channels)
+        scales = np.array(scales, dtype=np.int32)
+        for i in range(len(channels) - 1):
+            j = -i - 2
+            setattr(self, 'ida_{}'.format(i),
+                    IDAUp(3, channels[j], in_channels[j:],
+                          scales[j:] // scales[j]))
+            scales[j + 1:] = scales[j]
+            in_channels[j + 1:] = [channels[j] for _ in channels[j + 1:]]
+
+    def forward(self, inputs):
+        layers = list(inputs)
+        assert len(layers) > 1
+        for i in range(len(layers) - 1):
+            ida = getattr(self, 'ida_{}'.format(i))
+            x, y = ida(layers[-i - 2:])
+            layers[-i - 1:] = y
+        return x
+
+
+class DLASeg(nn.Module):
+    def __init__(self, base_name, heads, down_ratio=4, head_conv=256):
+        super(DLASeg, self).__init__()
+        self.heads = heads
+        self.first_level = int(np.log2(down_ratio))
+        self.base = DLASeg._get_base_block(base_name)
+        channels = self.base.channels
+        scales = [2 ** i for i in range(len(channels[self.first_level:]))]
+        self.dla_up = DLAUp(channels[self.first_level:], scales=scales)
+        for head in self.heads:
+            classes = self.heads[head]
+            if head_conv > 0:
+                fc = nn.Sequential(
+                    nn.Conv2d(in_channels=channels[self.first_level], out_channels=head_conv, kernel_size=(3, 3),
+                              stride=(1, 1), padding=1, bias=True),
+                    nn.ReLU(),
+                    nn.Conv2d(in_channels=head_conv, out_channels=classes, kernel_size=(1, 1), stride=(1, 1), padding=0,
+                              bias=True),
+                )
+            else:
+                fc = nn.Conv2d(in_channels=channels[self.first_level], out_channels=classes, kernel_size=(1, 1),
+                               stride=(1, 1), padding=0,
+                               bias=True)
+            self.__setattr__(head, fc)
+
+    @staticmethod
+    def _get_base_block(base_name):
+        if base_name == "dla34":
+            return DLA(levels=[1, 1, 1, 2, 2, 1], channels=[16, 32, 64, 128, 256, 512], block=BasicBlock,
+                       return_levels=True)
+        elif base_name == "dla60":
+            return DLA(levels=[1, 1, 1, 2, 3, 1], channels=[16, 32, 128, 256, 512, 1024], block=BottleNeck,
+                       return_levels=True)
+        elif base_name == "dla102":
+            return DLA(levels=[1, 1, 1, 3, 4, 1], channels=[16, 32, 128, 256, 512, 1024], block=BottleNeck,
+                       residual_root=True, return_levels=True)
+        elif base_name == "dla169":
+            return DLA(levels=[1, 1, 2, 3, 5, 1], channels=[16, 32, 128, 256, 512, 1024], block=BottleNeck,
+                       residual_root=True, return_levels=True)
+        else:
+            raise ValueError("The 'base_name' is invalid.")
+
+    def forward(self, x):
+        """
+        :param x: torch.Tensor, shape: (N, C, H, W)
+        :return: List of torch.Tensor, shape: [(N, num_classes, H/4, W/4), (N, 2, H/4, W/4), (N, 2, H/4, W/4)]
+        """
+        x = self.base(x)
+        x = self.dla_up(x[self.first_level:])
+        outputs = []
+        for head in self.heads:
+            outputs.append(self.__getattr__(head)(x))
+        return outputs
 
 
 # if __name__ == '__main__':
 #     sample = torch.randn(1, 3, 384, 384, dtype=torch.float32)
-#     model = DLA(levels=[1, 1, 1, 2, 2, 1], channels=[16, 32, 64, 128, 256, 512], block=BasicBlock,
-#                        return_levels=True)
+#     model = DLASeg(base_name="dla34", heads={"heatmap": 20, "wh": 2, "reg": 2})
 #     y = model(sample)
-#     for f in y:
-#         print(f.size())
-    # torch.Size([1, 16, 384, 384])
-    # torch.Size([1, 32, 191, 191])
-    # torch.Size([1, 64, 95, 95])
-    # torch.Size([1, 128, 47, 47])
-    # torch.Size([1, 256, 23, 23])
-    # torch.Size([1, 512, 11, 11])
+#     for o in y:
+#         print(o.size())

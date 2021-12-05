99c195fe805c9c4aa3f39bfd9fd36b90d4fc39ca
calmisential <xinyu_std@163.com>
2021-12-05 21:27:07 +0800

    Update

--- start_training.py
+++ start_training.py
@@ -3,12 +3,14 @@ import torch
 from load_yaml import load_yamls
 from scripts import CenterNetTrainer
 
-CONFIG = {
-    "model_name": "centernet",
-    "cfg": "centernet.yaml"
-}
 
 if __name__ == '__main__':
+    model_names = ["centernet"]
+    cfgs = ["centernet.yaml"]
+    CONFIG = {
+        "model_name": "centernet",
+        "cfg": "centernet.yaml"
+    }
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print("PyTorch version: {}, Device: {}".format(torch.__version__, device))
     cfg = load_yamls(model_yaml=CONFIG["cfg"], device=device)

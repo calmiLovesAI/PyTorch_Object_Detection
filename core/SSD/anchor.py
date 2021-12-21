a8f5fecba82af88679ce07ab2c01c23fca38cc46
calmisential <xinyu_std@163.com>
2021-12-21 17:25:20 +0800

    Different datasets use different default box scales.

--- core/SSD/anchor.py
+++ core/SSD/anchor.py
@@ -12,10 +12,19 @@ class DefaultBoxes:
         self.image_size = cfg["Train"]["input_size"]
 
         self.output_feature_sizes = cfg["Model"]["feature_size"]
-        self.default_boxes_sizes = cfg["Model"]["default_boxes_sizes"]
+        self.default_boxes_sizes = DefaultBoxes._get_default_boxes_sizes(cfg["Train"]["dataset_name"])
         self.aspect_ratios = get_aspect_ratio(cfg)
         self.num_priors = len(self.aspect_ratios)
         self.steps = cfg["Model"]["downsampling_ratio"]
+
+    @staticmethod
+    def _get_default_boxes_sizes(dataset):
+        if dataset == "voc":
+            return [[30, 60], [60, 111], [111, 162], [162, 213], [213, 264], [264, 315]]
+        elif dataset == "coco":
+            return [[21, 45], [45, 99], [99, 153], [153, 207], [207, 261], [261, 315]]
+        else:
+            raise NotImplementedError
 
     def __call__(self, *args, **kwargs):
         boxes = []

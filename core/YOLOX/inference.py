84badc69b108c9721a2139921c9cabc7f3663574
calmisential <xinyu_std@163.com>
2021-12-29 13:46:49 +0800

    Update

--- core/YOLOX/inference.py
+++ core/YOLOX/inference.py
@@ -1,7 +1,7 @@
-import numpy as np
-
 import torch
 import torchvision
+
+from utils.tools import reverse_direct_image_resize
 
 
 def postprocess(prediction, num_classes, conf_thre=0.7, nms_thre=0.45, class_agnostic=False):
@@ -51,18 +51,22 @@ def postprocess(prediction, num_classes, conf_thre=0.7, nms_thre=0.45, class_agn
     return output
 
 
-def get_specific_detection_results(dets):
+def get_specific_detection_results(dets, H, W, input_image_size):
     """
 
     Args:
         dets: torch.Tensor, shape: [N, 6(xmin, ymin, xmax, ymax, obj_conf, class_conf, class_pred)]
+        H: 待检测的图片原始高度
+        W: 待检测的图片原始宽度
+        input_image_size:
 
     Returns:
 
     """
     assert dets.dim() == 2
-    dets = dets.cpu().numpy()
-    boxes = dets[:, :4]
+    dets = dets.cpu()
+    boxes = reverse_direct_image_resize(h=H, w=W, input_size=input_image_size, boxes=dets[:, :4], xywh=False,
+                                        coords_normalized=True)
     scores = dets[:, 4] * dets[:, 5]
-    clses = dets[:, 6].astype(np.int32)
-    return boxes, scores, clses
+    clses = dets[:, 6].to(torch.int32)
+    return boxes.numpy(), scores.numpy(), clses.numpy()

1848622b3454399f2a3515fe86bf05fa826756e3
calmisential <xinyu_std@163.com>
2022-01-05 20:29:58 +0800

    Delete

--- core/SSD/anchor.py
+++ core/SSD/anchor.py
@@ -58,34 +58,3 @@ class DefaultBoxes:
             return anchors_ltrb
         else:
             return anchors
-
-        # boxes = []
-        # for k, f in enumerate(self.output_feature_sizes):
-        #     for i, j in product(range(f), repeat=2):
-        #         f_k = self.image_size / self.steps[k]
-        #         # box中心点的坐标
-        #         center_x = (j + 0.5) / f_k
-        #         center_y = (i + 0.5) / f_k
-        #         # box的高和宽
-        #         s_min = self.default_boxes_sizes[k][0] / self.image_size
-        #         boxes += [center_x, center_y, s_min, s_min]
-        #         # s_max = math.sqrt(self.default_boxes_sizes[k][0] * self.default_boxes_sizes[k][1]) / self.image_size
-        #         s_max = math.sqrt(s_min * (self.default_boxes_sizes[k][1] / self.image_size))
-        #
-        #         boxes += [center_x, center_y, s_max, s_max]
-        #         for ar in self.aspect_ratios[k]:
-        #             boxes += [center_x, center_y, s_min * math.sqrt(ar), s_min / math.sqrt(ar)]
-        #             boxes += [center_x, center_y, s_min / math.sqrt(ar), s_min * math.sqrt(ar)]
-        #
-        # anchors = torch.tensor(boxes, dtype=torch.float32)
-        # anchors = torch.reshape(anchors, shape=(-1, 4)).clamp_(min=0, max=1)   # shape: (8732, 4(cx, cy, w, h))
-        # anchors_ltrb = anchors.clone()   # (xmin, ymin, xmax, ymax)格式
-        # anchors_ltrb[:, 0] = anchors[:, 0] - 0.5 * anchors[:, 2]
-        # anchors_ltrb[:, 1] = anchors[:, 1] - 0.5 * anchors[:, 3]
-        # anchors_ltrb[:, 2] = anchors[:, 0] + 0.5 * anchors[:, 2]
-        # anchors_ltrb[:, 3] = anchors[:, 1] + 0.5 * anchors[:, 3]
-        #
-        # if xyxy:
-        #     return anchors_ltrb
-        # else:
-        #     return anchors
